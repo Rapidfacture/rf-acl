7740cf3913bc0bd8c6af4526507e6089fd93ff75
Felix Furtmayr <ff@rapidfacture.com>
2019-11-17 16:04:44 +0100

    use urls of erp, as login project will be removed. version 1.0.0

--- index.js
+++ index.js
@@ -209,7 +209,7 @@ module.exports.start = function (options, next) {
 
 
       function getBasicConfig (token, mainCallback) {
-         var loginUrls = config.global.apps['rf-app-login'].urls;
+         var loginUrls = config.global.apps['rf-app-erp'].urls;
          var basicInfo = {
             app: config.app,
             loginUrl: loginUrls.main + loginUrls.login,
